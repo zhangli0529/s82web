04787277524c72667711c0aefab7c1a4a2d3696d
张礼 <2071540671@qq.com>
2020-06-10 19:09:24 +0800

    张礼1

---
+++ s82webs/src/main/java/com/servlet/Servlet.java
@@ -0,0 +1,7 @@
+package com.servlet;
+
+public class Servlet {
+    public void zhangli(){
+        System.out.println("张礼项目学习小组");
+
+    }}
--- s82webs/src/main/java/com/servlet/Test.java
+++ s82webs/src/main/java/com/servlet/Test.java
@@ -2,7 +2,7 @@ package com.servlet;
 
 public class Test {
     public void zhangli(){
-        System.out.println("张礼项目小组");
+        System.out.println("是张礼项目小组");
     }
 
 
