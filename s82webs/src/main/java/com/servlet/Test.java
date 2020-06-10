19a463442febbbde79bebdde2db4c197e809f9a5
思怡呀 <206572658@qq.com>
2020-06-10 16:59:02 +0800

    Merge branch 'master' of D:\idea\s82web with conflicts.

--- s82webs/src/main/java/com/servlet/Test.java
+++ s82webs/src/main/java/com/servlet/Test.java
@@ -2,7 +2,6 @@ package com.servlet;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("dfe");
-        System.out.println("123");
+        System.out.println("aaa");
     }
 }
