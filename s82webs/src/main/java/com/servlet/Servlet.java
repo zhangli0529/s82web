ffa1bd1df93f50626d6f79cabef6e2543ac2b629
雷神 <2969484229@qq.com>
2020-06-10 19:13:27 +0800

    刘素明

--- s82webs/src/main/java/com/servlet/Servlet.java
+++ s82webs/src/main/java/com/servlet/Servlet.java
@@ -4,4 +4,10 @@ public class Servlet {
     public void zhangli(){
         System.out.println("张礼学习小组");
 
-    }}
+    }
+
+    public void liussuming(){
+        System.out.println("刘素明学习小组");
+
+    }
+}
--- s82webs/src/main/java/com/servlet/Tset2.java
+++ s82webs/src/main/java/com/servlet/Tset2.java
@@ -4,8 +4,6 @@ public class Tset2 {
 
     public static void main(String[] args) {
 
-
-
         System.out.println("太难了 ");
     }
 
