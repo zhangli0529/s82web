e6f185faace259ba61131400810839178d658b9d
雷神 <2969484229@qq.com>
2020-06-10 19:20:11 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	s82webs/src/main/java/com/servlet/Servlet.java
    #	s82webs/src/main/java/com/servlet/Tset2.java

--- s82webs/src/main/java/com/servlet/Servlet.java
+++ s82webs/src/main/java/com/servlet/Servlet.java
@@ -9,4 +9,7 @@ public class Servlet {
     public static void xxiejiaxin(String[] args) {
         System.out.println("谢家新学习小组");
     }
+    public static void lusuming(String[] args) {
+        System.out.println("刘素明学习小组");
+    }
 }
