a3282cda73bfe2cdcbabad57cac9438702c32e8a
chen <33376749473@qq.com>
2020-06-10 16:39:06 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	s82webs/src/main/java/com/servlet/Test.java
    #	s82webs/src/main/webapp/index.jsp

--- s82webs/src/main/java/com/servlet/Test.java
+++ s82webs/src/main/java/com/servlet/Test.java
@@ -3,8 +3,7 @@ package com.servlet;
 public class Test {
     public static void main(String[] args)
     {
-        System.out.println("eeeeeee");
-        System.out.println("阿汤本汤");
-        System.out.println("atang2");
+
+
     }
 }
